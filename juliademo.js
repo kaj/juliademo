268361b635c3120839eebe008466f6ba8ca4970e
Rasmus Kaj <kaj@kth.se>
2012-10-13 00:29:47 +0200

    Leave interior set unpainted.
    Silly optimization: compensate for the long calculations time of
    reaching maxiter by not painting those boxes, as the array are
    initialized to all zero, leaving unpainted pixles black (actually
    transparent, but I have a black bg).

--- juliademo.js
+++ juliademo.js
@@ -65,12 +65,15 @@ JuliaDemo.prototype = {
         for (var x = 0; x <= this.w - this.step; x += this.step) {
             for (var y = 0; y <= this.h - this.step; y += this.step) {
                 var z = new Complex(scale*(x-x0), scale*(y-y0));
-                var v = this.palette[this.v(z,c)];
-                for (var yy = this.step, i0 = y * this.w + x;
-                     yy > 0;
-                     --yy, i0 += ws) {
-                    for (var xx = this.step; xx > 0; --xx) {
-                        data[i0++] = v;
+                var n = this.v(z,c)
+                if (n < this.maxiter) {
+                    var v = this.palette[n];
+                    for (var yy = this.step, i0 = y * this.w + x;
+                         yy > 0;
+                         --yy, i0 += ws) {
+                        for (var xx = this.step; xx > 0; --xx) {
+                            data[i0++] = v;
+                        }
                     }
                 }
             }
